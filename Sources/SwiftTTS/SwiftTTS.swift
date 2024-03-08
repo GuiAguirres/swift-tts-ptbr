1ed45dba66207e55da1e6f6506743203e4e7b2e3
Guilherme Aguirres <aguirres.guilherme@icloud.com>
2024-03-08 16:41:34 -0300

    pt-BR lenguage

--- Sources/SwiftTTS/SwiftTTS.swift
+++ Sources/SwiftTTS/SwiftTTS.swift
@@ -90,7 +90,7 @@ private final class Engine: NSObject, AVSpeechSynthesizerDelegate {
 
 public extension SwiftTTS {
     static var live: Self {
-        let engine = Engine(rateRatio: 1.0, voice: AVSpeechSynthesisVoice(language: "en-GB"))
+        let engine = Engine(rateRatio: 1.0, voice: AVSpeechSynthesisVoice(language: "pt-BR"))
 
         let isSpeaking = AsyncStream { continuation in
             engine.isSpeaking = {
